9998d43724015311997f58f0d9171496d6f32060
itsupport <itsupport@tradelinksa.co.za>
2019-11-26 11:02:38 +0200

    Selection screen for Dye Order Reports

---
+++ DyeHouse/frmDyeOrdersSelection.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace DyeHouse
+{
+    partial class frmDyeOrdersSelection
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cmboColour = new DyeHouse.CheckComboBox();
+            this.cmboStyle = new DyeHouse.CheckComboBox();
+            this.cmboCustomers = new DyeHouse.CheckComboBox();
+            this.cmboGreige = new DyeHouse.CheckComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(128, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Quality ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(128, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Customers";
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(515, 381);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmit.TabIndex = 2;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(128, 180);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Style ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(128, 239);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Colour";
+            // 
+            // cmboColour
+            // 
+            this.cmboColour.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.cmboColour.FormattingEnabled = true;
+            this.cmboColour.Location = new System.Drawing.Point(260, 231);
+            this.cmboColour.Name = "cmboColour";
+            this.cmboColour.Size = new System.Drawing.Size(200, 21);
+            this.cmboColour.TabIndex = 8;
+            this.cmboColour.Text = "Select Options";
+            // 
+            // cmboStyle
+            // 
+            this.cmboStyle.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.cmboStyle.FormattingEnabled = true;
+            this.cmboStyle.Location = new System.Drawing.Point(260, 172);
+            this.cmboStyle.Name = "cmboStyle";
+            this.cmboStyle.Size = new System.Drawing.Size(200, 21);
+            this.cmboStyle.TabIndex = 7;
+            this.cmboStyle.Text = "Select Options";
+            // 
+            // cmboCustomers
+            // 
+            this.cmboCustomers.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.cmboCustomers.FormattingEnabled = true;
+            this.cmboCustomers.Location = new System.Drawing.Point(260, 113);
+            this.cmboCustomers.Name = "cmboCustomers";
+            this.cmboCustomers.Size = new System.Drawing.Size(200, 21);
+            this.cmboCustomers.TabIndex = 6;
+            this.cmboCustomers.Text = "Select Options";
+            // 
+            // cmboGreige
+            // 
+            this.cmboGreige.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.cmboGreige.FormattingEnabled = true;
+            this.cmboGreige.Location = new System.Drawing.Point(260, 59);
+            this.cmboGreige.Name = "cmboGreige";
+            this.cmboGreige.Size = new System.Drawing.Size(200, 21);
+            this.cmboGreige.TabIndex = 5;
+            this.cmboGreige.Text = "Select Options";
+            // 
+            // frmDyeOrdersSelection
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(617, 434);
+            this.Controls.Add(this.cmboColour);
+            this.Controls.Add(this.cmboStyle);
+            this.Controls.Add(this.cmboCustomers);
+            this.Controls.Add(this.cmboGreige);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmDyeOrdersSelection";
+            this.Text = "Please Select as appropriate";
+            this.Load += new System.EventHandler(this.frmDyeOrdersSelection_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private DyeHouse.CheckComboBox cmboGreige;
+        private DyeHouse.CheckComboBox cmboCustomers;
+        private DyeHouse.CheckComboBox cmboStyle;
+        private DyeHouse.CheckComboBox cmboColour;
+    }
+}
---
+++ DyeHouse/frmDyeOrdersSelection.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Utilities;
+
+namespace DyeHouse
+{
+    public partial class frmDyeOrdersSelection : Form
+    {
+        bool FormLoaded;
+
+        DyeHouse.DyeQueryParameters QueryParms;
+        DyeHouse.DyeRepository repo;
+
+        public frmDyeOrdersSelection()
+        {
+            InitializeComponent();
+            repo = new DyeRepository();
+
+            this.cmboGreige.CheckStateChanged += new System.EventHandler(this.cmboGreige_CheckStateChanged);
+            this.cmboStyle.CheckStateChanged += new System.EventHandler(this.cmboStyles_CheckStateChanged);
+            this.cmboColour.CheckStateChanged += new System.EventHandler(this.cmboColours_CheckStateChanged);
+            this.cmboCustomers.CheckStateChanged += new System.EventHandler(this.cmboCustomers_CheckStateChanged);
+        }
+
+        private void frmDyeOrdersSelection_Load(object sender, EventArgs e)
+        {
+            using (var context = new TTI2Entities())
+            {
+                FormLoaded = false;
+                QueryParms = new DyeQueryParameters();
+                FormLoaded = true;
+
+                var Grieges = context.TLADM_Griege.Where(x => !(bool)x.TLGriege_Discontinued).OrderBy(x => x.TLGreige_Description).ToList();
+                foreach (var Greige in Grieges)
+                {
+                    cmboGreige.Items.Add(new DyeHouse.CheckComboBoxItem(Greige.TLGreige_Id, Greige.TLGreige_Description, false));
+                }
+
+                var Styles = context.TLADM_Styles.OrderBy(x => x.Sty_Description).ToList();
+                foreach (var Style in Styles)
+                {
+                    cmboStyle.Items.Add(new DyeHouse.CheckComboBoxItem(Style.Sty_Id, Style.Sty_Description, false));
+                }
+
+                var Colours = context.TLADM_Colours.OrderBy(x => x.Col_Display).ToList();
+                foreach (var Colour in Colours)
+                {
+                    cmboColour.Items.Add(new DyeHouse.CheckComboBoxItem(Colour.Col_Id, Colour.Col_Display, false));
+                }
+
+                var Customers = context.TLADM_CustomerFile.Where(x=>!x.Cust_Blocked).OrderBy(x => x.Cust_Description).ToList();
+                foreach (var Customer in Customers)
+                {
+                    cmboCustomers.Items.Add(new DyeHouse.CheckComboBoxItem(Customer.Cust_Pk, Customer.Cust_Description, false));
+                }
+            }
+        }
+
+        private void cmboGreige_CheckStateChanged(object sender, EventArgs e)
+        {
+
+            if (sender is DyeHouse.CheckComboBoxItem && FormLoaded)
+            {
+                DyeHouse.CheckComboBoxItem item = (DyeHouse.CheckComboBoxItem)sender;
+                if (item.CheckState)
+                {
+                    QueryParms.Qualities.Add(repo.LoadQuality(item._Pk));
+
+                }
+                else
+                {
+                    var value = QueryParms.Qualities.Find(it => it.TLGreige_Id == item._Pk);
+                    if (value != null)
+                        QueryParms.Qualities.Remove(value);
+
+                }
+            }
+        }
+
+        private void cmboStyles_CheckStateChanged(object sender, EventArgs e)
+        {
+
+            if (sender is DyeHouse.CheckComboBoxItem && FormLoaded)
+            {
+                DyeHouse.CheckComboBoxItem item = (DyeHouse.CheckComboBoxItem)sender;
+                if (item.CheckState)
+                {
+                    QueryParms.Styles.Add(repo.LoadStyle(item._Pk));
+
+                }
+                else
+                {
+                    var value = QueryParms.Styles.Find(it => it.Sty_Id == item._Pk);
+                    if (value != null)
+                        QueryParms.Styles.Remove(value);
+
+                }
+            }
+        }
+
+        private void cmboColours_CheckStateChanged(object sender, EventArgs e)
+        {
+
+            if (sender is DyeHouse.CheckComboBoxItem && FormLoaded)
+            {
+                DyeHouse.CheckComboBoxItem item = (DyeHouse.CheckComboBoxItem)sender;
+                if (item.CheckState)
+                {
+                    QueryParms.Colours.Add(repo.LoadColour(item._Pk));
+
+                }
+                else
+                {
+                    var value = QueryParms.Colours.Find(it => it.Col_Id == item._Pk);
+                    if (value != null)
+                        QueryParms.Colours.Remove(value);
+
+                }
+            }
+        }
+
+        private void cmboCustomers_CheckStateChanged(object sender, EventArgs e)
+        {
+            if (sender is DyeHouse.CheckComboBoxItem && FormLoaded)
+            {
+                DyeHouse.CheckComboBoxItem item = (DyeHouse.CheckComboBoxItem)sender;
+                if (item.CheckState)
+                {
+                    QueryParms.Customers.Add(repo.LoadCustomer(item._Pk));
+
+                }
+                else
+                {
+                    var value = QueryParms.Customers.Find(it => it.Cust_Pk == item._Pk);
+                    if (value != null)
+                        QueryParms.Customers.Remove(value);
+
+                }
+            }
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                frmDyeViewReport vRep = new frmDyeViewReport(10, QueryParms);
+                int h = Screen.PrimaryScreen.WorkingArea.Height;
+                int w = Screen.PrimaryScreen.WorkingArea.Width;
+                vRep.ClientSize = new Size(w, h);
+                vRep.ShowDialog(this);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                cmboColour.Items.Clear();
+                cmboCustomers.Items.Clear();
+                cmboGreige.Items.Clear();
+                cmboStyle.Items.Clear();
+
+                this.frmDyeOrdersSelection_Load(this, null);
+            }
+
+        }
+    }
+}
